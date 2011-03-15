670f7d035ca9952ff0349fc75251f03aa0385938
rhodricusack <rhodricusack@gmail.com>
2011-03-15 13:34:06 +0000

    reorganisation

--- devel/cburealtime/matlab/cburt_dumpfigures.m
+++
@@ -1,9 +0,0 @@
-function [cburt]=cburt_dumpfigures(cburt,seriesnum)
-
-print('-f12','-r600','-depsc',fullfile(cburt.incoming.processeddata,sprintf('series%d_incomingdata',seriesnum)));
-print('-f14','-r600','-depsc',fullfile(cburt.incoming.processeddata,sprintf('series%d_modelling',seriesnum)));
-print('-f15','-r600','-depsc',fullfile(cburt.incoming.processeddata,sprintf('series%d_dai',seriesnum)));
-try
-    print('-f17','-r600','-depsc',fullfile(cburt.incoming.processeddata,sprintf('series%d_betasbyregion',seriesnum)));
-catch
-end;
