0eddf7d30c207118a5156e5966e17bec1e36e56f
rhodricusack <rhodricusack@gmail.com>
2011-03-15 13:31:59 +0000

    reorganisation

--- devel/apps/cburealtime/cburt_savecburt.m
+++
@@ -1,4 +0,0 @@
-function [cburt]=cburt_savecburt(cburt,seriesnum)
-
-save(fullfile(cburt.incoming.processeddata,'cburt.mat'),'cburt');
-save(fullfile(cburt.incoming.processeddata,sprintf('cburt_series%02d.mat',seriesnum)),'cburt');
