d43023d74e533ae2616902e330c063726e5860d4
rhodricusack <rhodricusack@gmail.com>
2011-03-17 07:51:29 +0000

    changed cburt_devel.m

--- cburt_devel.m
+++ cburt_devel.m
@@ -2,7 +2,7 @@ spmpth='/home/stefanh/realtime/spm8';
 global cburealtime_defaults
 cburealtime_defaults=[];
 cburealtime_defaults.path_data='/home/stefanh/realtime/scratch';
-cburealtime_defaults.path_code='/home/stefanh/realtime/svn/trunk/devel';
+cburealtime_defaults.path_code='/home/stefanh/realtime/svn/trunk';
 cburealtime_defaults.path_sambashare='/local/sambashare';
 
 % Names of your protocols as seen on the Siemens console. Use regular
